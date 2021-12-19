4d300cbd2268f0fca641e13d34aa6bd682bd11ad
ironzombi <span@ironzombi.com>
2021-12-19 18:58:36 +0800

    Initial commit
    A Hello World for Log4j and CVE-2021-44228

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -1,33 +1,25 @@
 package com.example.springboot;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application implements ApplicationRunner {
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("...........................Log4Shell Vulnerable Server..........................");
+        logger.info("a Maven Spring boot app(?)");
+        logger.warn("listens on port 8080");
+		logger.info("pass X-Api-Version");
+    }
 }
--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -1,24 +1,26 @@
 package com.example.springboot;
 
-import java.util.Arrays;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application {
+public class Application implements ApplicationRunner {
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
-	}
-	
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("Log4Shell Vulnerable Server");
+        logger.info("a Maven Spring boot app(?)");
+        logger.warn("listens on port 8080");
+		logger.warn("CVE-2021-44228 Log4j Remote Code Execution");
+		logger.info("pass X-Api-Version");
+    }
 }
